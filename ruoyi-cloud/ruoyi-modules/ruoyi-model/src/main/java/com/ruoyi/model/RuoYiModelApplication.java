2cfef62ae50f040b68dc1ded7ecf142dcb1ab0ad
chronoir1110specials-spec <chronoir1110specials@gmail.com>
2026-05-28 19:41:29 +0800

    feat: add ruoyi-agent, ruoyi-knowledge, ruoyi-model module skeleton

---
+++ ruoyi-cloud/ruoyi-modules/ruoyi-model/src/main/java/com/ruoyi/model/RuoYiModelApplication.java
@@ -0,0 +1,33 @@
+package com.ruoyi.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import com.ruoyi.common.security.annotation.EnableCustomConfig;
+import com.ruoyi.common.security.annotation.EnableRyFeignClients;
+
+/**
+ * 模型适配服务
+ *
+ * @author ruoyi
+ */
+@EnableCustomConfig
+@EnableRyFeignClients
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class RuoYiModelApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RuoYiModelApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  模型适配服务启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+}
